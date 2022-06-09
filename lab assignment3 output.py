7d90ba201fb54f86da85f6f0308bf8ed9f7cff89
palaksharma113 <104072764+palaksharma113@users.noreply.github.com>
2022-06-09 20:58:22 +0530

    Add files via upload

---
+++ lab assignment3 output.py
@@ -0,0 +1,32 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\kashish Aggarwal\AppData\Local\Programs\Python\Python310\lb assignment 3.py
+Enter a number
+4
+0b100
+100
+1 : addition
+2 : subtraction
+3 : multiplication
+4 : division
+enter your choice
+1
+Enter first number
+2
+Enter second number
+3
+2.0 + 3.0 = 5.0
+35
+45.0
+1256.6370614359173
+3.1622776601683795
+0.5
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+Enter number of Hydrogen Atoms: 2
+Enter number of Carbon Atoms: 1
+Enter number of Oxygen Atoms: 3
+Molecular weight of compound is: 62.02477999999999 grams/mole
