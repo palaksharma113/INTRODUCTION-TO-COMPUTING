2af8006fd2614487cca2e2c7352ff92b0a428ff7
palaksharma113 <104072764+palaksharma113@users.noreply.github.com>
2022-06-12 21:51:35 +0530

    Add files via upload

---
+++ lab assignment4 output.py
@@ -0,0 +1,29 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Gazalveer Kaur/AppData/Local/Programs/Python/Python310/lab assignment4'.py
+Enter Marks :85
+ Grade A 
+Enter a year: 2025
+year is not a Leap Year
+Question 1:10X10=100
+Right!
+Question 2:4X6=25
+Wrong.The right answer is 24
+Question 3:10X1=10
+Right!
+Question 4:5X1=56
+Wrong.The right answer is 5
+Question 5:7X3=31
+Wrong.The right answer is 21
+Question 6:3X3=9
+Right!
+Question 7:10X10=100
+Right!
+Question 8:10X7=69
+Wrong.The right answer is 70
+Question 9:6X8=48
+Right!
+Question 10:7X10=70
+Right!
+177 candies are in the bowl!
